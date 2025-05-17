88915c8cbbf520c3285c26953590154652bba2db
SamuelReyes6671 <samuel.reyes.condori@email.com>
2025-05-16 22:15:34 -0500

    updating get,post, put and delete

--- routes/users.js
+++ routes/users.js
@@ -7,4 +7,10 @@ router.get("/", usersController.getAll);
 
 router.get("/:id", usersController.getSingle);
 
+router.post("/", usersController.createUser);
+
+router.get("/:id", usersController.updateUser);
+
+router.delete("/:id", usersController.deleteUser);
+
 module.exports = router;
